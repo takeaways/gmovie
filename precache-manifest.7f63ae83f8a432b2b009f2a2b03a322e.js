d666f444467c73b37134bbedbaf3262df2977e76
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-04-24 00:55:58 +0900

    Updates

---
+++ precache-manifest.7f63ae83f8a432b2b009f2a2b03a322e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab414b94df23917b27e00f31a02e8a3",
+    "url": "/gmovie/index.html"
+  },
+  {
+    "revision": "5749549b5791c6d964e7",
+    "url": "/gmovie/static/js/2.10362dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gmovie/static/js/2.10362dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73bbb5a9e2b40a852a1",
+    "url": "/gmovie/static/js/main.1585d430.chunk.js"
+  },
+  {
+    "revision": "b2d9e328ee908a351b86",
+    "url": "/gmovie/static/js/runtime-main.28780227.js"
+  }
+]);
